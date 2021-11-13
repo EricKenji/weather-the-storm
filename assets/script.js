75fb6cebca5310c3dabb8fbee3266fe1116e16c4
Eric Furukawa <eric.furukawa@gmail.com>
2021-11-13 15:14:43 -0800

    added if else statement to change UV index colors

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,48 @@ var searchCity = function (event) {
     var cityName = searchEl.value.trim();
     if (cityName) {
         fetchCurrent(cityName);
+        storeCity(cityName);
+        addSearchHistory(cityName);
     }
 };
 
+
+// Create City Array
+
+var storeCity = function (city) {
+    citiesArray.push(city);
+};
+
+// add to search history
+var addSearchHistory = function (cityName) {
+    var historyBtn = document.createElement("button");
+    historyBtn.innerHTML = cityName;
+    document.getElementById("button1").appendChild(historyBtn);
+};
+
+// set City names to local storage
+var namesInStorage = localStorage.getItem("city-names");
+var citiesArray;
+if (namesInStorage === null) {
+    citiesArray = [];
+} else {
+    citiesArray = JSON.parse(namesInStorage);
+};
+for (var i = 0; i < citiesArray.length; i++) {
+    addSearchHistory(citiesArray[i]);
+}
+
+
+
 // fetch API data
 var fetchCurrent = (city) => {
-    var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=9488ec8c097fd6f500089eda4c1d7cef";
+    var apiCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=9488ec8c097fd6f500089eda4c1d7cef";
 
-    fetch(apiCall).then(function (response) {
+    fetch(apiCurrent).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayCurrent(data);
+                fetchUv(data);
                 console.log(data);
             });
         } else {
@@ -34,6 +65,7 @@ var displayCurrent = function(data) {
     var currentCityEl = document.querySelector(".current-city");
     var iconEl = document.querySelector("#icon");
     var statusEl = document.querySelector("#status");
+    var dateEl = document.querySelector("#date");
     var temperatureEl = document.querySelector("#temp");
     var windspeedEl = document.querySelector("#windspeed");
     var humidityEl = document.querySelector("#humidity");
@@ -43,11 +75,48 @@ var displayCurrent = function(data) {
     // Display icon and status
     iconEl.setAttribute("src", "https://openweathermap.org/img/wn/"+ data.weather[0].icon + "@2x.png");
     statusEl.textContent = data.weather[0].main;
+    // Display date using Moment 
+    dateEl.textContent = " " + moment().format('MMMM Do YYYY');
     // change temperature, windspeed, and humidity
-    temperatureEl.textContent = data.main.temp;
-    windspeedEl.textContent = data.wind.speed;
-    humidityEl.textContent = data.main.humidity;
+    temperatureEl.textContent = data.main.temp + " F";
+    windspeedEl.textContent = data.wind.speed + " MPH";
+    humidityEl.textContent = data.main.humidity + "%";
 };
+
+// fetch API UV index
+var fetchUv = function (data) {
+    var apiUv = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&APPID=9488ec8c097fd6f500089eda4c1d7cef";
+    // Fetch API URL
+    fetch(apiUv).then(function (response) {
+            response.json().then(function (data) {
+                console.log(data);
+                displayUv(data);;
+
+            });
+    });
+};
+
+// Display UV Index
+var displayUv = function (data) {
+    var uvIndexEl = document.querySelector("#uv-index");
+    console.log(data.current.uvi)
+    uvIndexEl.textContent = 15;
+
+    // Change color of UV Index 
+    if (parseInt(uvIndexEl.textContent) < 3) {
+        uvIndexEl.className = "low";
+    } else if (3 <= parseInt(uvIndexEl.textContent) && parseInt(uvIndexEl.textContent) < 5) {
+        uvIndexEl.className = "moderate";
+    } else if (5 <= parseInt(uvIndexEl.textContent) && parseInt(uvIndexEl.textContent) < 7) {
+        uvIndexEl.className = "high";
+    } else if (7 <= parseInt(uvIndexEl.textContent) && parseInt(uvIndexEl.textContent) < 10) {
+        uvIndexEl.className = "very-high";
+    } else if (parseInt(uvIndexEl.textContent) > 11) {
+        uvIndexEl.className = "extreme";
+    }
+};
+
+
 
 
 
